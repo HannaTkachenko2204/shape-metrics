0563100a9ddea81793a2d3e8297d56162938e5f5
Анна <anna.kuvakina91@gmail.com>
2025-05-29 17:39:55 +0300

    jest-circle

--- parser.test.js
+++ parser.test.js
@@ -1,7 +1,21 @@
 import { parseLine } from "./parser";
 import Square from "./shapes/square";
+import Shape from "./shapes/shape";
+import Circle from "./shapes/circle";
 
 test ('test-sqwere', () => {
     const sqwere = parseLine('Square TopRight 1 1 Side 1');
     expect(sqwere).toBeInstanceOf(Square);
+    expect(sqwere).toBeInstanceOf(Shape);
+    expect(sqwere.side).toBe(1);
 })
+
+test ('test-circle', () => {
+    const circle = parseLine('Circle Center 1 1 Radius 2');
+    expect(circle).toBeInstanceOf(Circle);
+    expect(circle).toBeInstanceOf(Shape);
+    expect(circle.radius).toBe(2);
+})
+
+
+
