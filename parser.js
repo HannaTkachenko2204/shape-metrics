ebedea0fe072f7802ba9c6098bc451a0d813628f
Анна <anna.kuvakina91@gmail.com>
2025-05-29 18:57:21 +0300

    over

--- parser.js
+++ parser.js
@@ -1,7 +1,7 @@
-import Square from './shapes/square.js';
-import Rectangle from './shapes/rectangle.js';
-import Circle from './shapes/circle.js';
-import Triangle from './shapes/triangle.js';
+import Square from "./shapes/square.js";
+import Rectangle from "./shapes/rectangle.js";
+import Circle from "./shapes/circle.js";
+import Triangle from "./shapes/triangle.js";
 
 function getParts(line) {
   const parts = line.trim().split(/\s+/);
@@ -9,36 +9,25 @@ function getParts(line) {
 }
 
 // функция parseLine(line) по строке создаёт нужный объект
-export function parseLine(line) { // line — строка из файла input.txt
-  const parts = line.trim().split(/\s+/); // удаляем пробелы по краям и разбиваем строку по пробелам в массив parts
+export function parseLine(line) {
+  // line — строка из файла input.txt
+  const parts = getParts(line); // удаляем пробелы по краям и разбиваем строку по пробелам в массив parts
   const shapeType = parts[0]; // первый элемент (parts[0]) — это тип фигуры: 'Square', 'Rectangle', 'Circle', 'Triangle'
 
-  switch (shapeType) { // разбор по типу фигуры:
-    case 'Square': return parserSquare(line)
+  switch (
+    shapeType // разбор по типу фигуры:
+  ) {
+    case "Square":
+      return parserSquare(line);
 
-    case 'Rectangle': {
-      // Rectangle TopRight 2 2 BottomLeft 1 1
-      const topRightIndex = parts.indexOf('TopRight');
-      const bottomLeftIndex = parts.indexOf('BottomLeft');
-      if (topRightIndex === -1 || bottomLeftIndex === -1)
-        throw new Error('Rectangle missing TopRight or BottomLeft parameter');
-      const x1 = Number(parts[topRightIndex + 1]);
-      const y1 = Number(parts[topRightIndex + 2]);
-      const x2 = Number(parts[bottomLeftIndex + 1]);
-      const y2 = Number(parts[bottomLeftIndex + 2]);
-      return new Rectangle(x1, y1, x2, y2);
-    }
+    case "Rectangle":
+      return parserRectangle(line);
 
-    case 'Circle': return parserCircle(line)
+    case "Circle":
+      return parserCircle(line);
 
-    case 'Triangle': {
-      const coords = parts
-        .filter((_, i) => i >= 2) // пропускаем первые два слова (Triangle Point1)
-        .map(Number) // преобразуем элементы в числа
-        .filter(n => !isNaN(n)); // отфильтровываем нечисловые элементы
-      if (coords.length !== 6) throw new Error("Triangle requires 3 points"); // проверяем, что чисел ровно 6 (по 2 координаты на 3 точки)
-      return new Triangle(...coords);
-    }
+    case "Triangle":
+      return parserTriangle(line);
 
     default:
       throw new Error(`Unknown shape type: ${shapeType}`); // если строка начинается с неизвестного слова
@@ -48,23 +37,49 @@ export function parseLine(line) { // line — строка из файла input
 export function parserSquare(line) {
   const parts = getParts(line);
 
-  const sideIndex = parts.indexOf('Side');
-      if (sideIndex === -1) throw new Error('Square missing Side parameter');
-      const side = Number(parts[sideIndex + 1]);
-      return new Square(side);
+  const sideIndex = parts.indexOf("Side");
+  if (sideIndex === -1) throw new Error("Square missing Side parameter");
+  const side = Number(parts[sideIndex + 1]);
+
+  return new Square(side);
 }
 
 export function parserCircle(line) {
   const parts = getParts(line);
 
-  const centerIndex = parts.indexOf('Center');
-      const radiusIndex = parts.indexOf('Radius');
-      if (centerIndex === -1 || radiusIndex === -1)
-      throw new Error('Circle missing Center or Radius parameter');
+  const centerIndex = parts.indexOf("Center");
+  const radiusIndex = parts.indexOf("Radius");
+  if (centerIndex === -1 || radiusIndex === -1)
+    throw new Error("Circle missing Center or Radius parameter");
+  const x = Number(parts[centerIndex + 1]);
+  const y = Number(parts[centerIndex + 2]);
+  const radius = Number(parts[radiusIndex + 1]);
 
-      const x = Number(parts[centerIndex + 1]);
-      const y = Number(parts[centerIndex + 2]);
-      const radius = Number(parts[radiusIndex + 1]);
+  return new Circle(x, y, radius);
+}
 
-      return new Circle(x, y, radius);
+export function parserRectangle(line) {
+  const parts = getParts(line);
+
+  const topRightIndex = parts.indexOf("TopRight");
+  const bottomLeftIndex = parts.indexOf("BottomLeft");
+  if (topRightIndex === -1 || bottomLeftIndex === -1)
+    throw new Error("Rectangle missing TopRight or BottomLeft parameter");
+  const x1 = Number(parts[topRightIndex + 1]);
+  const y1 = Number(parts[topRightIndex + 2]);
+  const x2 = Number(parts[bottomLeftIndex + 1]);
+  const y2 = Number(parts[bottomLeftIndex + 2]);
+
+  return new Rectangle(x1, y1, x2, y2);
+}
+
+export function parserTriangle(line) {
+  const parts = getParts(line);
+
+  const coords = parts
+    .filter((_, i) => i >= 2) // пропускаем первые два слова (Triangle Point1)
+    .map(Number) // преобразуем элементы в числа
+    .filter((n) => !isNaN(n)); // отфильтровываем нечисловые элементы
+  if (coords.length !== 6) throw new Error("Triangle requires 3 points"); // проверяем, что чисел ровно 6 (по 2 координаты на 3 точки)
+  return new Triangle(...coords);
 }
--- parser.test.js
+++ parser.test.js
@@ -2,12 +2,14 @@ import { parseLine } from "./parser";
 import Square from "./shapes/square";
 import Shape from "./shapes/shape";
 import Circle from "./shapes/circle";
+import Rectangle from "./shapes/rectangle";
+import Triangle from "./shapes/triangle";
 
-test ('test-sqwere', () => {
-    const sqwere = parseLine('Square TopRight 1 1 Side 1');
-    expect(sqwere).toBeInstanceOf(Square);
-    expect(sqwere).toBeInstanceOf(Shape);
-    expect(sqwere.side).toBe(1);
+test ('test-square', () => {
+    const square = parseLine('Square TopRight 1 1 Side 1');
+    expect(square).toBeInstanceOf(Square);
+    expect(square).toBeInstanceOf(Shape);
+    expect(square.side).toBe(1);
 })
 
 test ('test-circle', () => {
@@ -17,5 +19,24 @@ test ('test-circle', () => {
     expect(circle.radius).toBe(2);
 })
 
+test ('test-rectangle', () => {
+    const rectangle = parseLine('Rectangle TopRight 2 2 BottomLeft 1 1');
+    expect(rectangle).toBeInstanceOf(Rectangle);
+    expect(rectangle).toBeInstanceOf(Shape);
+    expect(rectangle.width).toBe(1);
+    expect(rectangle.height).toBe(1);
+})
+
+test('test-triangle', () => {
+    const triangle = parseLine('Triangle Point1 0 0 Point2 4 0 Point3 0 3');
+    expect(triangle).toBeInstanceOf(Triangle);
+    expect(triangle).toBeInstanceOf(Shape);
+    expect(triangle.p1.x).toBe(0);
+    expect(triangle.p1.y).toBe(0);
+    expect(triangle.p2.x).toBe(4);
+    expect(triangle.p2.y).toBe(0);
+    expect(triangle.p3.x).toBe(0);
+    expect(triangle.p3.y).toBe(3);
+});
 
 
